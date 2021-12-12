9eba382c4102af6939ec25d72d2ea5869e99be0b
bronte <widely13@gmail.com>
2021-12-12 22:19:54 +0800

    測試跟新增pm25資料完成

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
+import pandas as pd
 
 webObj=Flask(__name__) # main.py
 
@@ -11,11 +12,20 @@ def frontPage():
     time=today()
     return render_template('index.html',**locals())
 
+@webObj.route('/pm25', methods=['GET', 'POST'])
+def pm25():
+    url = 'https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=csv'
+    df = pd.read_csv(url)[['county', 'Site', 'PM25', 'ItemUnit']]
+
+    columns = df.columns
+    values = df.values.tolist()
+    time = today()
+
+    return render_template('pm25.html', **locals())
+
 @webObj.route('/today')
 def today():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 webObj.run(debug=True)
-
-
 
