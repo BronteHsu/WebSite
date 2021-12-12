760572878947a20ae60f3fcbc72d0bb43ec6b408
bronte <widely13@gmail.com>
2021-12-12 20:54:15 +0800

    修改index.html 使用template

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 webObj=Flask(__name__) # main.py
 
 # 建立route
 
-# 首頁
-@webObj.route('/')
+@webObj.route('/') # 首頁
 def frontPage():
-    return 'Hello!'
+    name='bronte'
+    time=today()
+    return render_template('index.html',**locals())
 
 @webObj.route('/today')
 def today():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 webObj.run(debug=True)
+
+
+
